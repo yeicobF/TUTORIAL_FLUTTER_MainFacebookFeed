661fb525343b261adb095582d75c4248322c3a8c
yeicobF <60598673+yeicobF@users.noreply.github.com>
2021-06-06 19:49:16 -0500

    INICIO SliverAppBar - AppBar superior

--- src/lib/screens/home_screen.dart
+++ src/lib/screens/home_screen.dart
@@ -3,13 +3,60 @@
  */
 
 import 'package:flutter/material.dart';
+import '../config/palette.dart';
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomScrollView(
+        slivers: [
+          // SliverAppBar:
+          // - Scroll hacia abajo -> Se esconde la barra superior.
+          // - Scroll hacia arriba -> Se muestra la barra superior.
+          SliverAppBar(
+            // Se cambian los colores de los elementos de la barra de
+            // notificaciones.
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            centerTitle: false,
+            // Para no tener que hacer scroll hasta arriba para que se vea la
+            // AppBar.
+            floating: true,
+            backwardsCompatibility: true,
+            // Para que se vea una sombra debajo de la AppBar.
+            // forceElevated: true,
+            title: const Text(
+              "facebook",
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            // actions: Para hacer funcionar a los botones.
+            actions: [
+              // Para simular el círculo gris de alrededor.
+              Container(
+                margin: const EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.search),
+                  iconSize: 30.0,
+                  color: Colors.black,
+                  // Lo que se ejecutará cuando el botón sea presionado.
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
