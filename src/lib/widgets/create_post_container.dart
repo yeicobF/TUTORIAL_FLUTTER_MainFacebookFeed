9467581bbd8b04749f120c64b088fba49ebc9f9b
yeicobF <60598673+yeicobF@users.noreply.github.com>
2021-06-07 05:21:37 -0500

    CreatePost TERMINADO - Con TextField y Botones
    Ya está terminada la sección para crear una publicación. Puedes ingresar
    texto y los botones son funcionales.

--- src/lib/widgets/create_post_container.dart
+++ src/lib/widgets/create_post_container.dart
@@ -18,8 +18,11 @@ class CreatePostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0)
+      // - fromLTRB: Left, Top, Right, Bottom.
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
       // height: 100.0,
-      color: Colors.orange,
+      color: Colors.white,
       child: Column(
         children: [
           Row(
@@ -32,8 +35,13 @@ class CreatePostContainer extends StatelessWidget {
                 backgroundImage:
                     CachedNetworkImageProvider(currentUser.imageUrl),
               ),
+              // Agregamos un espacio entre la foto de perfil y el texto.
+              // const: No se vuelve a renderizar.
+              const SizedBox(
+                width: 8.0,
+              ),
               // Que tome todo el espacio disponible.
-              Expanded(
+              const Expanded(
                 // Campo de texto.
                 child: TextField(
                   // Decoración del input del TextField.
@@ -45,6 +53,65 @@ class CreatePostContainer extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+          // Divider: Línea divisora.
+          const Divider(
+            height: 10.0,
+            thickness: 0.5,
+          ),
+          // Fila en donde se encuentran 3 botones: Live, Photo, Room.
+          Container(
+            // Altura definida. Esto provoca que el VerticalDivider se vea.
+            height: 40.0,
+            child: Row(
+              // Separar los elementos entre sí.
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // FlatButton.icon: FUE DEPRECADO POR TextButton.icon.
+                // Botón con un ícono y texto.
+                TextButton.icon(
+                  onPressed: () => print("Live"),
+                  icon: const Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: const Text(
+                    "Live",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                // VerticalDivider: Dividor vertical que no se ve, solo pone un
+                // espacio entre los elementos.
+                const VerticalDivider(
+                  width: 8.0,
+                ),
+                TextButton.icon(
+                  onPressed: () => print("Photo"),
+                  icon: const Icon(
+                    Icons.photo_library,
+                    color: Colors.green,
+                  ),
+                  label: const Text(
+                    "Photo",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const VerticalDivider(
+                  width: 8.0,
+                ),
+                TextButton.icon(
+                  onPressed: () => print("Room"),
+                  icon: const Icon(
+                    Icons.video_call,
+                    color: Colors.purpleAccent,
+                  ),
+                  label: const Text(
+                    "Room",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
